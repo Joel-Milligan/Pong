5b2c5256bec3e40af1f3e3dd372caec87ab03ade
Joel <22701252@student.uwa.edu.au>
2020-01-21 21:49:56 +0800

    Created game clock

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ screen_size = (800, 600)
 # Initialisation
 pygame.init()
 main_display = pygame.display.set_mode(screen_size)
+clock = pygame.time.Clock()
 
 # Functions
 def update_screen():
@@ -21,6 +22,7 @@ while running:
             running = False
 
     update_screen()
+    clock.tick(60)
 
 pygame.quit()
 quit()
