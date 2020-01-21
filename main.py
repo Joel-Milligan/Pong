5b071f29ba09312168c01f8708f7b4545c7634c8
Joel <22701252@student.uwa.edu.au>
2020-01-21 21:00:49 +0800

    main.py skeleton

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+import colours
+
+# Variables
+running = True
+screen_size = (800, 600)
+
+# Initialisation
+pygame.init()
+main_display = pygame.display.set_mode(screen_size)
+
+# Functions
+def update_screen():
+    main_display.fill(colours.black)
+
+# Main Game Loop
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    update_screen()
+
+pygame.quit()
+quit()
