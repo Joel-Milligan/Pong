2d2d481816acfaff94a0ac14197397476a900587
Joel <22701252@student.uwa.edu.au>
2020-01-21 23:18:50 +0800

    Created init

--- ball.py
+++ ball.py
@@ -1,6 +1,16 @@
 import pygame
 import colours
+import math
 
 class Ball:
-    def __init__(self):
-        pass
+    radius = 10
+
+    def __init__(self, screen, x, y):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.colour = colours.white
+        self.speed = 5
+        self.unit_vector = [1/math.sqrt(2), 1/math.sqrt(2)]
+        self.velocity = [i * self.speed for i in self.unit_vector]
+
