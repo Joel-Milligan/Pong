72c533aea9df0886fb7a5486280b3d35ceab1edb
Joel <22701252@student.uwa.edu.au>
2020-01-21 23:20:52 +0800

    Create game ball

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import colours
 import paddle
+import ball
 
 # Variables
 running = True
@@ -11,6 +12,7 @@ indent = 50
 pygame.init()
 main_display = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
+game_ball = ball.Ball(main_display, screen_size[0] / 2, screen_size[1] / 2)
 player1 = paddle.Paddle(main_display, \
                         screen_size[0] - (indent + paddle.Paddle.width), \
                         (screen_size[1] / 2) - (paddle.Paddle.length - 2))
