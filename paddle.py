c6507acd0c478a1864d2fa43d7dbbef0a1733cb3
Joel <22701252@student.uwa.edu.au>
2020-01-22 15:17:18 +0800

    Made paddle a proper rect

--- paddle.py
+++ paddle.py
@@ -3,25 +3,23 @@ import colours
 
 class Paddle:
     # Constant Variables
-    length = 100
+    height = 100
     width = 20
     colour = colours.white
     speed = 5
     
     def __init__(self, screen, x, y):
         # Parameter Variables
-        self.x = x
-        self.y = y
+        self.rect = pygame.Rect(x, y, self.width, self.height)
         self.screen = screen
 
     def move_down(self):
-        if(self.y + self.length < self.screen.get_height()):
-            self.y += self.speed
+        if(self.rect[1] + self.rect[3] < self.screen.get_height()):
+            self.rect.y += self.speed
 
     def move_up(self):
-        if(self.y > 0):
-            self.y -= self.speed
+        if(self.rect[1] > 0):
+            self.rect.y -= self.speed
 
     def draw(self):
-        rect = (self.x, self.y, self.width, self.length)
-        pygame.draw.rect(self.screen, self.colour, rect)
+        pygame.draw.rect(self.screen, self.colour, self.rect)
