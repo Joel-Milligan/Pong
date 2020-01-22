522deaeeceb6344cd1f40aeb849272268ef343f8
Joel <22701252@student.uwa.edu.au>
2020-01-22 15:15:51 +0800

    Changed ball to a rect

--- ball.py
+++ ball.py
@@ -9,13 +9,14 @@ class Ball:
         self.screen = screen
         self.x = x
         self.y = y
+        self.rect = pygame.Rect(x - self.radius, y - self.radius, self.radius * 2, self.radius * 2)
         self.colour = colours.white
         self.speed = 5
         self.unit_vector = [1/math.sqrt(2), 1/math.sqrt(2)]
         self.velocity = [i * self.speed for i in self.unit_vector]
 
     def draw(self):
-        pygame.draw.circle(self.screen, self.colour, (int(self.x), int(self.y)), int(self.radius))
+        pygame.draw.circle(self.screen, self.colour, (self.rect[0], self.rect[1]), int(self.radius))
 
     def calculate_velocity(self):
         self.velocity = [i * self.speed for i in self.unit_vector]
@@ -25,5 +26,5 @@ class Ball:
         calculate_velocity()
 
     def update_position(self):
-        self.x += self.velocity[0]
-        self.y += self.velocity[1]
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
