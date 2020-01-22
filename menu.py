270b83f51ddc67b8890d8906d279a605c0cc68b7
Joel <22701252@student.uwa.edu.au>
2020-01-22 16:23:21 +0800

    Delete menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,9 @@
 import pygame
 import colours
 
+# Font Initialisation
+menu_font = pygame.font.SysFont("arial", 72)
 
+# Create all the text objects
+start_game_text = menu_font.render("Start Game", True, colours.white)
+quit_text = menu_font.render("Quit", True, colours.white)
